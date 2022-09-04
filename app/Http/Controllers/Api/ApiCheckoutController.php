c6885869b3a3cf296bdb1235afaefac9b3ab344d
wahidalqorni <idunalqorni@gmail.com>
2022-09-04 10:31:39 +0700

    first commit

---
+++ app/Http/Controllers/Api/ApiCheckoutController.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Checkout;
+use App\Models\Keranjang;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class ApiCheckoutController extends Controller
+{
+
+    public function postCheckout(Request $request)
+    {
+        // ambil total harga yg ada di tabel keranjangs yg statusnya adalah 0 = keranjang, 1 = checkout 
+        $getTotalHarga = DB::table('keranjangs')
+            ->select(DB::raw('SUM(totalharga) as totalharga '))
+            ->where('status', '0')
+            ->where('user_id', $request->user_id)
+            ->first();
+        // var_dump($getTotalHarga->totalharga);
+        // die;
+        if ($getTotalHarga->totalharga == NULL) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Keranjang Kosong!',
+                    'data' => []
+                ],
+                500
+            );
+            die;
+        }
+
+        $ongkir = 25000;
+
+        // Db transaction => akan mengupdate data apabila proses 1 berhasil dilakukan
+        // jika proses pertama/kedua saat di proses ketiga gagal, maka proses 1 dan 2 dibatalkan
+        // proses yg akan dilakukan : 1. update data yg ada di tabel detailpemesanas (checkout_id)
+        // 2. insert data baru ke tabel checkouts
+
+        // opening db transaction
+        DB::beginTransaction();
+
+        try {
+            // isi proses
+            // 1. proses insert data ke tabel checkouts
+            $postCheckout = Checkout::create([
+                'kode_transaksi' => 'STP-' . Str::random(5),
+                'user_id' => $request->user_id,
+                'nama' => $request->nama,
+                'nohp' => $request->nohp,
+                'alamat' => $request->alamat,
+                'kota_kecamatan' => $request->kota_kecamatan,
+                'catatan' => $request->catatan,
+                'jenis_pembayaran' => $request->jenis_pembayaran,
+                'jenis_pengiriman' => $request->jenis_pengiriman,
+                'ongkir' => $ongkir,
+                'grand_total' => $getTotalHarga->totalharga + $ongkir
+            ]);
+
+            // 2. proses update checkout_id di tabel keranjangs
+            // ambil dulu id pada tabel checkouts yg barusan disimpan ke database
+            $checkout_id = $postCheckout->id;
+
+            // proses update tabel keranjangs pd field checkout_id dan status
+
+            // yg bakal diupdate adlh yg statusnya masih keranjang (0)
+            $updateDetail = Keranjang::where('status', '0')->where('user_id', $request->user_id)->update([
+                'checkout_id' => $checkout_id,
+                'status' => '1'
+            ]);
+
+            // success transaction
+            DB::commit();
+
+            // response success
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => 'Post Data Berhasil',
+                    'data' => $postCheckout
+                ],
+                200
+            );
+        } catch (Exception $error) {
+
+            // saat gagal, maka cancel smua transaction data
+            DB::rollBack();
+
+            // response gagal
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Post Data Gagal ' . $error->getMessage(),
+                    'data' => []
+                ],
+                500
+            );
+        }
+    }
+
+    public function getCheckoutBaru()
+    {
+        try {
+            $data = Checkout::where('user_id', request('user_id'))->where('status', '0')->get();
+            // response success
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => 'Get Data Berhasil',
+                    'data' => $data
+                ],
+                200
+            );
+        } catch (Exception $error) {
+            // response gagal
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Get Data Gagal ' . $error->getMessage(),
+                    'data' => []
+                ],
+                500
+            );
+        }
+    }
+
+    public function getCheckoutProses()
+    {
+        try {
+            $data = Checkout::where('user_id', request('user_id'))->where('status', '1')->get();
+            // response success
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => 'Get Data Berhasil',
+                    'data' => $data
+                ],
+                200
+            );
+        } catch (Exception $error) {
+            // response gagal
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Get Data Gagal ' . $error->getMessage(),
+                    'data' => []
+                ],
+                500
+            );
+        }
+    }
+
+    public function getCheckoutSelesai()
+    {
+        try {
+            $data = Checkout::where('user_id', request('user_id'))->where('status', '2')->get();
+            // response success
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => 'Get Data Berhasil',
+                    'data' => $data
+                ],
+                200
+            );
+        } catch (Exception $error) {
+            // response gagal
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Get Data Gagal ' . $error->getMessage(),
+                    'data' => []
+                ],
+                500
+            );
+        }
+    }
+
+    public function getCheckoutDetail()
+    {
+        try {
+            $detail = Checkout::where('user_id', request('user_id'))->where('id', request('id_pemesanan'))->first();
+            $products = DB::table('keranjangs')
+                ->join('products', 'products.id', 'keranjangs.product_id')
+                ->join('merks', 'merks.id', 'products.merk_id')
+                ->select(DB::raw(
+                    'keranjangs.*, products.nama_product, products.gambar, products.harga as harga_satuan, merks.merk_product'
+                ))
+                ->where('keranjangs.checkout_id', request('id_pemesanan'))
+                ->get();
+
+            // response success
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => 'Get Data Berhasil',
+                    'detail' => $detail,
+                    'data' => $products
+                ],
+                200
+            );
+        } catch (Exception $error) {
+            // response gagal
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Get Data Gagal ' . $error->getMessage(),
+                    'detail' => $detail,
+                    'data' => $products
+                ],
+                500
+            );
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/product-rekomendasi', [App\Http\Controllers\Api\ApiProductController::class, 'getRekomendasi']);
+Route::get('/product-list', [App\Http\Controllers\Api\ApiProductController::class, 'getAllProduct']);
+Route::get('/product-new', [App\Http\Controllers\Api\ApiProductController::class, 'getNewProduct']);
+Route::get('/product-search',[App\Http\Controllers\Api\ApiProductController::class, 'searchProduct' ]);
+Route::get('/product-detail/{id}', [App\Http\Controllers\Api\ApiProductController::class,'detailProduct']);
+
+Route::post('/keranjang-post', [App\Http\Controllers\Api\ApiKeranjangController::class, 'postKeranjang']);
+Route::get('/keranjang-list', [App\Http\Controllers\Api\ApiKeranjangController::class, 'getAllKeranjang']);
+Route::post('/keranjang-delete', [App\Http\Controllers\Api\ApiKeranjangController::class, 'deleteKeranjang']);
+
+Route::post('/checkout-post', [App\Http\Controllers\Api\ApiCheckoutController::class, 'postCheckout']);
+Route::get('/checkout-list-baru', [App\Http\Controllers\Api\ApiCheckoutController::class, 'getCheckoutBaru']);
+Route::get('/checkout-list-proses', [App\Http\Controllers\Api\ApiCheckoutController::class, 'getCheckoutProses']);
+Route::get('/checkout-list-selesai', [App\Http\Controllers\Api\ApiCheckoutController::class, 'getCheckoutSelesai']);
+Route::get('/checkout-detail', [App\Http\Controllers\Api\ApiCheckoutController::class, 'getCheckoutDetail']);
+Route::post('/register', [App\Http\Controllers\Api\ApiUserController::class, 'register']);
+Route::post('/login', [App\Http\Controllers\Api\ApiUserController::class, 'login']);
+Route::get('/logout', [App\Http\Controllers\Api\ApiUserController::class, 'logout']);
