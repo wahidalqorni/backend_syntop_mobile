501b1538aca479de5b9cd730a485ada835879316
M. Wahid Alqorni <alqorni@Ms-MacBook-Pro.local>
2024-08-01 15:06:55 +0700

    update checkout post by id

--- app/Http/Controllers/Api/ApiCheckoutController.php
+++ app/Http/Controllers/Api/ApiCheckoutController.php
@@ -104,6 +104,96 @@ class ApiCheckoutController extends Controller
         }
     }
 
+    public function postCheckoutById(Request $request)
+    {
+        // ambil total harga yg ada di tabel keranjangs yg statusnya adalah 0 = keranjang, 1 = checkout 
+        $getTotalHarga = DB::table('keranjangs')
+            ->select(DB::raw('SUM(totalharga) as totalharga '))
+            ->where('status', '0')
+            ->where('user_id', $request->user_id)
+            ->first();
+        // var_dump($getTotalHarga->totalharga);
+        // die;
+        if ($getTotalHarga->totalharga == NULL) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Keranjang Kosong!',
+                    'data' => []
+                ],
+                500
+            );
+            die;
+        }
+
+        $ongkir = 25000;
+
+        // Db transaction => akan mengupdate data apabila proses 1 berhasil dilakukan
+        // jika proses pertama/kedua saat di proses ketiga gagal, maka proses 1 dan 2 dibatalkan
+        // proses yg akan dilakukan : 1. update data yg ada di tabel detailpemesanas (checkout_id)
+        // 2. insert data baru ke tabel checkouts
+
+        // opening db transaction
+        DB::beginTransaction();
+
+        try {
+            // isi proses
+            // 1. proses insert data ke tabel checkouts
+            $postCheckout = Checkout::create([
+                'kode_transaksi' => 'STP-' . Str::random(5),
+                'user_id' => $request->user_id,
+                'nama' => $request->nama,
+                'nohp' => $request->nohp,
+                'alamat' => $request->alamat,
+                'kota_kecamatan' => $request->kota_kecamatan,
+                'catatan' => $request->catatan,
+                'jenis_pembayaran' => $request->jenis_pembayaran,
+                'jenis_pengiriman' => $request->jenis_pengiriman,
+                'ongkir' => $ongkir,
+                'grand_total' => $getTotalHarga->totalharga + $ongkir
+            ]);
+
+            // 2. proses update checkout_id di tabel keranjangs
+            // ambil dulu id pada tabel checkouts yg barusan disimpan ke database
+            $checkout_id = $postCheckout->id;
+
+            // proses update tabel keranjangs pd field checkout_id dan status
+
+            // yg bakal diupdate adlh yg statusnya masih keranjang (0)
+            $updateDetail = Keranjang::where('status', '0')->where('user_id', $request->user_id)->update([
+                'checkout_id' => $checkout_id,
+                'status' => '1'
+            ]);
+
+            // success transaction
+            DB::commit();
+
+            // response success
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => 'Post Data Berhasil',
+                    'data' => $postCheckout
+                ],
+                200
+            );
+        } catch (Exception $error) {
+
+            // saat gagal, maka cancel smua transaction data
+            DB::rollBack();
+
+            // response gagal
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Post Data Gagal ' . $error->getMessage(),
+                    'data' => []
+                ],
+                500
+            );
+        }
+    }
+
     public function getCheckoutBaru()
     {
         $datas = [];
